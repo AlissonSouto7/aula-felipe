ae7bd1e73ff4b5f23aef3cf38f7a8208e658006e
Alisson Souto <141871349+AlissonSouto7@users.noreply.github.com>
2026-03-03 23:58:21 +0000

    adicioa arquivo `.editorconfig`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Célia, je t'aime de tout mon cœur. Tu es tellement belle que j'en oublie mes mots... alors si tu m'aimes aussi, fais-moi une petite risette pour me rassurer !</h1>
+    return <h1>Célia, je t'aime de tout mon cœur. Tu es tellement belle que j'en oublie mes mots... alors si tu m'aimes aussi, fais-moi une petite risette pour me rassurer!</h1>
 }
 
 export default Home;
